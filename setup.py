88e34a2e942001dcbf01476d6a6c06f5f62aacfd
soorajgeo <soorajgeo@gmail.com>
2024-01-08 19:46:53 +0530

    Added 7 more minerals to REE_data

--- setup.py
+++ setup.py
@@ -7,12 +7,20 @@ build_options = {'packages': [], 'excludes': [], 'include_files': ['REE_data.csv
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
 
+
+
 executables = [
-    Executable('ree.py', base=base, target_name = 'reesearch', icon='crystal.ico')
+    Executable('ree.py', base=base, target_name = 'reesearch', icon='crystal.ico'),
 ]
+
+
+
 
 setup(name='REEsearch',
       version = '1.0',
       description = 'Searches REE minerals from webmineral database given the composition data',
-      options = {'build_exe': build_options},
-      executables = executables)
+      options = {
+        'build_exe': build_options
+        },
+      executables = executables,
+      )
